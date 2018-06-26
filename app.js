32d657e020fd83f1b196c75d4dacef2780666508
Christian Rekdal <rekdal.christian@gmail.com>
2018-06-26 15:36:07 +0200

    Added functinality to RSVP page

--- app.js
+++ app.js
@@ -2,24 +2,37 @@ const form = document.getElementById('registrar');
 const input = form.querySelector('input');
 const ul = document.getElementById('invitedList');
 
+function createLi(text) {
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const text = input.value;
-  input.value = "";
+  //Appends a listItem with checkbox and label
   const li = document.createElement('li');
   li.textContent = text;
   const label = document.createElement('label');
   label.textContent = 'Confirmed';
+
   const checkbox = document.createElement('input');
   checkbox.type = 'checkbox';
   label.appendChild(checkbox);
   li.appendChild(label);
 
-  const button = document.createElement('button');
-  button.textContent = 'remove';
-  li.appendChild(button);
+  //Create edit button
+  const editButton = document.createElement('button');
+  editButton.textContent = 'edit';
+  li.appendChild(editButton);
 
+  //Create a remove button
+  const removeButton = document.createElement('button');
+  removeButton.textContent = 'remove';
+  li.appendChild(removeButton);
+
+  return li;
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const text = input.value;
+  input.value = "";
+  const li = createLi(text);
   ul.appendChild(li);
 });
 
@@ -37,8 +50,12 @@ ul.addEventListener('change', (e) => {
 
 ul.addEventListener('click', (e) => {
   if (e.target.tagName === 'BUTTON') {
+    if (e.target.textContent === 'remove') {
       const li = e.target.parentNode;
       const ul = li.parentNode;
       ul.removeChild(li);
+    } else if(e.target.textContent === 'edit') {
+      
+    }
   }
 });
